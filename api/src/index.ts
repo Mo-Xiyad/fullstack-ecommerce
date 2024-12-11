16194e1727ac9158ab20da2811978b3a6ec1159d
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-12-11 14:29:38 +0100

    json parsing

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,11 @@
-import express from 'express';
+import express, { json, urlencoded } from 'express';
 import productsRouter from './routes/products/index';
+
 const app = express();
 const port = process.env.PORT;
+
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
 app.use('/products', productsRouter);
 
--- api/src/routes/products/handlers.ts
+++ api/src/routes/products/handlers.ts
@@ -9,6 +9,7 @@ export function updateProductById(req: Request, res: Response) {
   res.send('update products by id');
 }
 export function createProduct(req: Request, res: Response) {
+  console.log(req.body);
   res.send('create products');
 }
 export function deleteProduct(req: Request, res: Response) {
