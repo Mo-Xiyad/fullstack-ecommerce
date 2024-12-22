6b91321a2d27b840a719f67a00af3726817e07a3
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-12-22 16:57:26 +0100

    auth api

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,17 +1,34 @@
-import ProductListItem from '@/components/ProductListItem';
+import { listProducts } from '@/api/products';
+import { Text } from '@/components/ui/text';
 import { useBreakpointValue } from '@/components/ui/utils/use-break-point-value';
-import { FlatList } from 'react-native';
-import products from '../assets/products.json';
+import { useQuery } from '@tanstack/react-query';
+import { ActivityIndicator, FlatList } from 'react-native';
+import ProductListItem from '../components/ProductListItem';
+
 export default function HomeScreen() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['products'],
+    queryFn: listProducts
+  });
+
   const numColumns = useBreakpointValue({
     default: 2,
     sm: 3,
     xl: 4
   });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Error fetching products</Text>;
+  }
+
   return (
     <FlatList
       key={numColumns}
-      data={products.splice(0, 10)}
+      data={data.splice(0, 10)}
       numColumns={numColumns}
       contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
       columnWrapperClassName="gap-2"
