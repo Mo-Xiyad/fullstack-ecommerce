a2f3ab964130c34eaaaa16448355e8f4b8e6fb60
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2024-12-22 16:29:13 +0100

    stack screens

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,13 +1,49 @@
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import { Icon } from '@/components/ui/icon';
+import { Text } from '@/components/ui/text';
 import '@/global.css';
+import { useAuth } from '@/store/authStore';
+import { useCart } from '@/store/cartStore';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Stack } from 'expo-router';
+import { Link, Stack } from 'expo-router';
+import { ShoppingCart, User } from 'lucide-react-native';
+import { Pressable } from 'react-native';
 const queryClient = new QueryClient();
 export default function RootLayout() {
+  const cartItemsNum = useCart((s) => s.items.length);
+  const isLoggedIn = useAuth((s) => !!s.token);
   return (
     <QueryClientProvider client={queryClient}>
       <GluestackUIProvider mode="light">
-        <Stack />
+        <Stack
+          screenOptions={{
+            headerRight: () =>
+              cartItemsNum > 0 && (
+                <Link href={'/cart'} asChild>
+                  <Pressable className="flex-row gap-2">
+                    <Icon as={ShoppingCart} />
+                    <Text>{cartItemsNum}</Text>
+                  </Pressable>
+                </Link>
+              )
+          }}
+        >
+          <Stack.Screen
+            name="index"
+            options={{
+              title: 'Shop',
+              headerLeft: () =>
+                !isLoggedIn && (
+                  <Link href={'/login'} asChild>
+                    <Pressable className="flex-row gap-2">
+                      <Icon as={User} />
+                    </Pressable>
+                  </Link>
+                )
+            }}
+          />
+          <Stack.Screen name="product/[id]" options={{ title: 'Product' }} />
+        </Stack>
       </GluestackUIProvider>
     </QueryClientProvider>
   );
